a8d4cf9099af284f727582dfaff3652fcb5063ec
charlstown <carlosgranden@gmail.com>
2025-05-12 01:05:26 +0200

    update: red button on hover

--- src/components/table/TableRow.jsx
+++ src/components/table/TableRow.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import IconDeleteRow from "../../assets/IconDeleteRow";
 
 function TableRow({ row, columns, rowIndex, onRowDelete }) {
   return (
@@ -8,7 +9,7 @@ function TableRow({ row, columns, rowIndex, onRowDelete }) {
         .map(({ field, highlight }) => (
           <td
             key={field}
-            className={`p-4 text-sm text-slate-700 ${
+            className={`p-2 text-sm text-slate-700 ${
               highlight ? "bg-teal-50" : "bg-white"
             }`}
           >
@@ -29,19 +30,10 @@ function TableRow({ row, columns, rowIndex, onRowDelete }) {
       <td className="p-4 text-sm text-slate-700">
         <button
           type="button"
-          className="text-slate-500 hover:text-slate-700"
+          className="text-slate-500 hover:text-red-500"
           onClick={() => onRowDelete(rowIndex)}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="2"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
+          <IconDeleteRow />
         </button>
       </td>
     </tr>
